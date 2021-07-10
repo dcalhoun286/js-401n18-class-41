af08ebd63926eb8941cc7eee4633e97740508455
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-07-09 22:48:18 -0700

    edited styling on title

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View } from 'react-native';
+import { Animated } from 'react-native';
 import { NativeBaseProvider, Box, Row } from 'native-base';
+import { LinearGradient } from 'expo-linear-gradient';
 
+// import { Card } from 'react-native-paper';
 import Steps from './pedometer.js';
 import styles from './styles.js';
 
@@ -11,7 +14,7 @@ export default function App () {
   return (
     <NativeBaseProvider>
       <View style={styles.title}>
-        <Text>iHeartHealth</Text>
+        <Text style={{fontSize: 35, fontWeight: 'bold'}}>iHeartHealth</Text>
         <StatusBar style="auto" />
       </View>
       <View style={styles.pedometer}>
